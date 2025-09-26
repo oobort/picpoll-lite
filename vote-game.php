40f56ee4864eed6e937cb555744d3c6d2a99cbab
oobort <6n34n8k34g78lt67vgfyui7kuk7ty7@gmail.com>
2025-09-27 02:02:16 +0800

    Update vote-game.php

--- vote-game.php
+++ vote-game.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: PicPoll Photo Voting Game
- * Text Domain: picpoll-lite-main
+ * Text Domain: picpoll-photo-voting-game
  * Description: Flexible photo voting game with regions, categories, dynamic vote options, styling options, and a shortcode builder.
  * Version: 1.8.14
  * Author: LeadMuffin
